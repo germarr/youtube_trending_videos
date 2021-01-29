7160d37bb2e602e37726481484a4f168ba7b9703
Geardo M <57111135+germarr@users.noreply.github.com>
2021-01-29 11:47:00 -0600

    Update get_video_list.py

--- get_video_list.py
+++ get_video_list.py
@@ -27,6 +27,28 @@ chart_mx= youtube.videos().list(
     maxResults=50
 
     # The maxResults parameter specifies how many results you want to be returned in the call. 
-    # I choose 50 because that's the max number you can get per call. 
+    # I choose 50 because that's the max nuber you can get per call. 
 
     ).execute()
+
+for i in chart_mx["items"]:
+    vid_id=i["id"]
+    ytb_link=f"https://youtu.be/{vid_id}"
+    
+    top_videos.append({
+        "published_date":i["snippet"]["publishedAt"],
+        "trending_date":f"{date_new}T{hour}:00:00Z",
+        "category_id":i["snippet"]["categoryId"],
+        "channel_title":i["snippet"]["channelTitle"],
+        "video_title":i["snippet"]["title"],
+        "views":i["statistics"]["viewCount"], 
+        "likes":i["statistics"]["likeCount"],
+        "dislikes":i["statistics"]["dislikeCount"],
+        "comments":i["statistics"].get("commentCount"),
+        "description":i["snippet"]["description"],
+        "channel_id":i["snippet"]["channelId"],
+        "link":ytb_link,
+        "thumbnail":i["snippet"]["thumbnails"]["medium"]["url"]
+    })
+
+pd.DataFrame.from_dict(top_videos).to_csv(f"{title}.csv")
